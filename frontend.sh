efa29a7e16d2a421c3d88e9f770d4790688a7fcb
om <omejjigiri99@gmail.com>
2023-11-04 20:42:52 +0530

    making shell scripting topics - little heading underlining

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,6 @@
 dnf install nginx -y
 
-systemctl enable nginx
-
-systemctl start nginx
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 rm -rf /usr/share/nginx/html/*
 
@@ -12,5 +10,10 @@ cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+systemctl enable nginx
+
+systemctl start nginx
+
+
+
 
