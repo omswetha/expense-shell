b3eeee5a4b9aadb499da47815045df8be5ad7d53
om <omejjigiri99@gmail.com>
2023-11-06 20:53:26 +0530

    making shell scripting topics - little heading underlining

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,27 @@
 
 echo -e "\e[36m installing the nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/expense.log
 
 echo -e "\e[36m enabling the nginx \e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>/tmp/expense.log
 
 echo -e "\e[36m starting the nginx \e[0m"
-systemctl start nginx
+systemctl start nginx &>>/tmp/expense.log
 
 echo -e "\e[36m copying the expense.conf file \[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
 echo -e "\e[36m removing the unwanted informatiom \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 echo -e "\e[36m downloading the expense \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[36m moving to the directory \e[0m"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>/tmp/expense.log
 
 echo -e "\e[36m unzipping the file \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 
 
