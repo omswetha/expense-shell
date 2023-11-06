a4584f9e498cfad15615da6b10a7e103dd856617
om <omejjigiri99@gmail.com>
2023-11-06 22:31:26 +0530

    making shell scripting topics - little heading underlining

--- backend.sh
+++ backend.sh
@@ -2,71 +2,71 @@ log_file=/tmp/expens.log
 colour="\e[33m"
 
 echo -e "${colour} disable the nodejs \e[0m"
-dnf module disable nodejs -y &>>$log_log_file
+dnf module disable nodejs -y &>>$log_file
 echo $?
 
 echo -e "${colour} enable the nodejs \e[0m"
-dnf module enable nodejs:18 -y &>>$log_log_file
+dnf module enable nodejs:18 -y &>>$log_file
 echo $?
 
 echo -e "${colour} install the nodejs \e[0m"
-dnf install nodejs -y &>>$log_log_file
+dnf install nodejs -y &>>$log_file
 echo $?
 
 echo -e "${colour} copy the backend.service \e[0m"
-cp backend.service /etc/systemd/system/backend.service &>>$log_log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 echo $?
 
 echo -e "${colour} Adding expense user \e[0m"
-useradd expense &>>$log_log_file
+useradd expense &>>$log_file
 echo $?
 
 echo -e "${colour} creating the application directory \e[0m"
-mkdir /app &>>$log_log_file
+mkdir /app &>>$log_file
 echo $?
 
 echo -e "${colour} creating the application directory \e[0m"
-rm -rf /app/* &>>$log_log_file
+rm -rf /app/* &>>$log_file
 echo $?
 
 echo -e "${colour} downloading the backend application \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_log_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 echo $?
 
 echo -e "${colour} moving to the application \e[0m"
-cd /app &>>$log_log_file
+cd /app &>>$log_file
 echo $?
 
 echo -e "${colour} unzipping the backend \e[0m"
-unzip /tmp/backend.zip &>>$log_log_file
+unzip /tmp/backend.zip &>>$log_file
 echo $?
 
 echo -e "${colour} moving to the directory \e[0m"
-cd /app &>>$log_log_file
+cd /app &>>$log_file
 echo $?
 
 echo -e "${colour} installing the dependencies of nodejs \e[0m"
-npm install &>>$log_log_file
+npm install &>>$log_file
 echo $?
 
 echo -e "${colour} reloading the system \e[0m"
-systemctl daemon-reload &>>$log_log_file
+systemctl daemon-reload &>>$log_file
 echo $?
 
 echo -e "${colour} enabling the backend \e[0m"
-systemctl enable backend &>>$log_log_file
+systemctl enable backend &>>$log_file
 echo $?
 
 echo -e "${colour} starting the backend \e[0m"
-systemctl start backend &>>$log_log_file
+systemctl start backend &>>$log_file
 echo $?
 
 echo -e "${colour} installing the mysql client \e[0m"
-dnf install mysql -y &>>$log_log_file
+dnf install mysql -y &>>$log_file
 echo $?
 
 echo -e "${colour} creating the schema \e[0m"
-mysql -h mysql-dev.osdevops99.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_log_file
+mysql -h mysql-dev.osdevops99.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 echo $?
 
 
