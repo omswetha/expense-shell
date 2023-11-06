5b8515779676c846f3400b6cceeca526861685f7
om <omejjigiri99@gmail.com>
2023-11-06 21:59:20 +0530

    making shell scripting topics - little heading underlining

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,28 @@
+log_file=/tmp/expens.log
+colour="\e[36m"
 
-echo -e "\e[36m installing the nginx \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+echo -e "${colour} installing the nginx \e[0m"
+dnf install nginx -y &>>$log_file
 
-echo -e "\e[36m enabling the nginx \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
+echo -e "${colour} enabling the nginx \e[0m"
+systemctl enable nginx &>>$log_file
 
-echo -e "\e[36m starting the nginx \e[0m"
-systemctl start nginx &>>/tmp/expense.log
+echo -e "${colour} starting the nginx \e[0m"
+systemctl start nginx &>>$log_file
 
-echo -e "\e[36m copying the expense.conf file \[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+echo -e "${colour} copying the expense.conf file \[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
-echo -e "\e[36m removing the unwanted informatiom \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+echo -e "${colour} removing the unwanted informatiom \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
-echo -e "\e[36m downloading the expense \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+echo -e "${colour} downloading the expense \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+echo -e "${colour} moving to the directory \e[0m"
+cd /usr/share/nginx/html &>>$log_file
 
-echo -e "\e[36m moving to the directory \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-
-echo -e "\e[36m unzipping the file \e[0m"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo -e "${colour} unzipping the file \e[0m"
+unzip /tmp/frontend.zip &>>$log_file
 
 
 
